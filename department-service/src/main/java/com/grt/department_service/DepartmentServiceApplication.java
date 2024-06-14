9e312a01f64c8a65b344cf986b48879a08c882aa
Raviteja Gandam <gandamraviteja@gmail.com>
2024-06-14 15:38:31 +0530

    PArtial dev

--- department-service/src/main/java/com/grt/department_service/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/grt/department_service/DepartmentServiceApplication.java
@@ -2,9 +2,10 @@ package com.grt.department_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
